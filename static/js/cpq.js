15af81251043ab7eb02e13714c33d5adb8b6e99a
jorgebarcelos <jorgebarcelos.faria@gmail.com>
2023-10-06 12:17:53 -0300

    feat: user session, signin, signup & signout

--- static/js/cpq.js
+++ static/js/cpq.js
@@ -2,9 +2,9 @@
 const updateBtns = document.getElementsByClassName('update-cart')
 
 for (let index = 0; index < updateBtns.length; index++) {
-  updateBtns[index].addEventListener('click', function () {
-    const productID = this.dataset.product
-    const action = this.dataset.action
-    console.log('product', productID, 'action', action)
-  })
+    updateBtns[index].addEventListener('click', function () {
+        const productID = this.dataset.product
+        const action = this.dataset.action
+        console.log('product:', productID, 'action:', action)
+    })
 }
