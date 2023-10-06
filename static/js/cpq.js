3d0fe0c875b7af170b9ee47b4df1fc57434932db
jorgebarcelos <jorgebarcelos.faria@gmail.com>
2023-10-06 14:03:39 -0300

    :grab logged user in js

--- static/js/cpq.js
+++ static/js/cpq.js
@@ -6,5 +6,12 @@ for (let index = 0; index < updateBtns.length; index++) {
         const productID = this.dataset.product
         const action = this.dataset.action
         console.log('product:', productID, 'action:', action)
+        console.log('User:', user)
+
+        if (user === 'AnonymousUser') {
+            console.log('Not Logged in')
+        }else{
+            console.log('Logged!!!')
+        }
     })
 }
